e5a64ed7b80972b614781d728ac3375ec0b35037
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-01-08 21:59:30 +0530

    session 1

--- session1/index.js
+++ session1/index.js
@@ -1,11 +1,38 @@
-console.log("hello world");
+// console.log("hello world");
 
-localStorage.setItem("JAN--2024", "hello jan class");
+// localStorage.setItem("JAN--2024", "hello jan class");
 
-const a = 1;
-const b = 2;
-const sum = a + b;
+// const a = 1;
+// const b = 2;
+// const sum = a + b;
 
-const obj = {};
+// const obj = {};
 
-console.log(sum);
+// console.log(sum);
+
+//  ------- - - - - - - - -
+
+// Api
+
+const news = "Someone Won gold medal"; // fetch("hindustan.com")
+
+// selecting single element
+const headingElement = document.getElementById("heading2");
+console.dir(headingElement);
+headingElement.innerHTML = news;
+
+const querySelectorElement = document.querySelector("#heading1"); // It will give a single NODE
+console.log(querySelectorElement);
+
+// Selecting multiple element
+
+const headingElementBytag = document.getElementsByTagName("h1");
+console.log(headingElementBytag);
+
+const headingElementByClassName = document.getElementsByClassName("punjabi");
+console.log(headingElementByClassName);
+
+const querySelectorElements = document.querySelectorAll("h1");
+console.log(querySelectorElements);
+
+querySelectorElement.textContent = `<u><b>woops!!!</b></u    asadsadsadjgasjdga         asdgasjdgasjd g`;
